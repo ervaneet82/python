717e57274be77fe5790e6d13346634e534c600c4
Vaneet Gupta <ervaneet82@gmail.com>
2018-02-20 07:52:55 +0530

    Understanding Tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+details=("Vaneet","Gupta","36","Trusting Social")
+
+firstName,lastName,Age,Company=details
+print("firstName : {}".format(firstName))
+print("lastName : {}".format(lastName))
+print("Age : {}".format(Age))
+print("Company : {}".format(Company))
