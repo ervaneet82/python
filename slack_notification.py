863a8cbe71cabe12b965c0ba1aa2dd65e68b7539
Vaneet Gupta <ervaneet82@gmail.com>
2019-08-29 12:28:34 +0530

    Remove api

--- slack_notification.py
+++ slack_notification.py
@@ -2,7 +2,7 @@ from slacker import Slacker
 
 
 def lambda_handler(event=None, context=None):
-    slack = Slacker("xoxp-472938759794-473990142295-706606643280-483e92f55e785059742ca166a0f13070")
+    slack = Slacker("")
     if slack.api.test().successful:
         print(f"Connected to {slack.team.info().body['team']['name']}.")
     else:
